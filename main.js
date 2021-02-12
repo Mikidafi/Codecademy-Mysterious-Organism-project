e514814fdaed7e476b804412c1d4135c95331722
Mikidafi <mchl.str84@gmail.com>
2021-02-12 23:20:22 +0100

    factory function ready

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -12,11 +12,19 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+// Since you need to create multiple objects, create a factory function pAequorFactory() that has two parameters:
 
+// The first parameter is number (no two organisms should have the same number).
+// The second parameter is an array of 15 DNA bases.
+// pAequorFactor() should return an object that contains the properties specimenNum and dna that correspond to the parameters provided.
 
+// You’ll also add more methods to this returned object in the later steps.
 
-
-
-
-
-
+// Factory function
+function pAequorFactor(number, arr) {
+  return {
+    specimenNum: number,
+    dna: arr,
+  };
+}
+console.log(pAequorFactor(4, mockUpStrand()));
